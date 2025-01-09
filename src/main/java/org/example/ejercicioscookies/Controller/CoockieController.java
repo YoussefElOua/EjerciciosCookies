c5a5b599df50c197a626e2c7ee14938ba1e0e142
YoussefElOua <poromarte@gmail.com>
2025-01-09 10:26:42 +0100

    .

--- src/main/java/org/example/ejercicioscookies/Controller/CoockieController.java
+++ src/main/java/org/example/ejercicioscookies/Controller/CoockieController.java
@@ -26,45 +26,45 @@ public class CoockieController {
     }
     @GetMapping("/name/create")
     public String createNameForm(HttpSession session, Model model) {
-        // Agregar un nuevo atributo vacío al modelo
+
         Persona persona = (Persona) session.getAttribute("persona");
         if (persona == null) {
             persona = new Persona();
             session.setAttribute("persona", persona);
         }
         model.addAttribute("person", persona.getName());
-        return "login"; // Renderizar createName.html
+        return "login";
     }
 
     @PostMapping("/name/save")
     public String saveName(@RequestParam("name") String name, HttpSession session) {
-        // Obtener la instancia de Persona desde la sesión
+
         Persona persona = (Persona) session.getAttribute("persona");
         if (persona == null) {
             persona = new Persona();
         }
 
-        // Actualizar el nombre
+
         persona.setName(name);
         session.setAttribute("persona", persona);
 
-        // Redirigir a la página que muestra el nombre
+
         return "redirect:/name";
     }
 
     @GetMapping("/name")
     public String showName(HttpSession session, Model model) {
-        // Obtener la persona desde la sesión
+
         Persona persona = (Persona) session.getAttribute("persona");
 
-        // Validar si hay un nombre
+
         if (persona == null || persona.getName() == null || persona.getName().isEmpty()) {
-            return "redirect:/name/create"; // Redirigir si no hay un nombre
+            return "redirect:/name/create";
         }
 
-        // Agregar el nombre al modelo
+
         model.addAttribute("person", persona.getName());
-        return "name"; // Renderizar showName.html
+        return "name";
     }
 
 
